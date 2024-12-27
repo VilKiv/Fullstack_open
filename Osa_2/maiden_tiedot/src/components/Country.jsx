67b5a1c39dc09304535b9d339a234b6d165fe519
Ville <ville.kivioja@tuni.fi>
2024-12-27 16:33:26 +0200

    Refaktoroitu koodia

--- Osa_2/maiden_tiedot/src/components/Country.jsx
+++ Osa_2/maiden_tiedot/src/components/Country.jsx
@@ -1,30 +1,21 @@
 import CountryDetails from "./CountryDetails"
 
 const Country = ({country,toggleDetails}) => {
-    if (!country) {
-        return <></>
-    }
-
     const label = !country.show
                 ? "show"
                 : "hide"
 
     const countryId = country.cca3
-    if (!country.show) {
-        return (
-            <div>
-                {country.name.common}
-                <button onClick={()=> {return toggleDetails(countryId)}}>{label}</button>
-            </div>
-        ) 
-    }
     return (
         <div>
             {country.name.common}
             <button onClick={()=> {return toggleDetails(countryId)}}>{label}</button>
-            <CountryDetails country={country}/>
+            {country.show
+                ? (<CountryDetails country={country}/>)
+                : <></>
+            }
         </div>
-    )
+    ) 
 }
 
 export default Country
